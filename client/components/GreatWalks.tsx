e6d21c3a535cdbc303f2e45a7b6dfe4568e5f971
Daisy Choi <chj15937@gmail.com>
2025-04-10 11:47:19 +1200

    styling for greatWalks page

--- client/components/GreatWalks.tsx
+++ client/components/GreatWalks.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 // import { useGreakWalks } from '../hooks/useFruits'
 import { Link } from 'react-router-dom'
 function GreatWalks() {
@@ -50,46 +51,71 @@ function GreatWalks() {
   }
   return (
     <>
-      {' '}
-      <div>
-        {' '}
-        <h1>Great Walks</h1>{' '}
-        <p>
-          {' '}
-          Filter By Difficulty:{' '}
-          <button onClick={intermediateGreatWalks}>Intermediate</button>{' '}
-          <button onClick={easyGreatWalks}>Easy</button>{' '}
-        </p>{' '}
+      <div className="flex flex-col justify-center items-center mt-10 ">
+        <h1 className="text-[60px] font-bold">Great Walks</h1>
+        <div className="gap-6 flex flex-row mt-6">
+          <button
+            onClick={easyGreatWalks}
+            className="p-0 m-0 border-none bg-transparent cursor-pointer"
+            aria-label="Toggle menu" // Good for screen readers
+          >
+            <img
+              src="client/components/assets/icons/easy.svg"
+              alt="Icon"
+              className="w-40"
+            />
+          </button>
+          <button
+            onClick={intermediateGreatWalks}
+            className="p-0 m-0 border-none bg-transparent cursor-pointer"
+            aria-label="Toggle menu" // Good for screen readers
+          >
+            <img
+              src="client/components/assets/icons/intermediate.svg"
+              alt="Icon"
+              className="w-40"
+            />
+          </button>
+        </div>
+
         <ul>
-          {' '}
           {greatWalks &&
             greatWalks.map((greatWalk) => (
-              <div key={greatWalk.id}>
-                {' '}
-                <li>
-                  {' '}
+              <div
+                key={greatWalk.id}
+                className="bg-[#1e293b]/60 drop-shadow-[0px_4px_136.6px_rgba(255,255,255,0.1)] px-10 py-4 my-10  mx-6 rounded-[45px]"
+              >
+                <li className="flex flex-row my-6 justify-center gap-6 text-[#d0f7a2]">
                   <Link to={`/great-walks/${greatWalk.id}`}>
-                    {' '}
                     <img
                       src={greatWalk.trackImageUrl}
                       alt={greatWalk.name}
-                    />{' '}
-                  </Link>{' '}
-                  <Link to={`/great-walks/${greatWalk.id}`}>
-                    {' '}
-                    <h1>{greatWalk.name}</h1>{' '}
-                  </Link>{' '}
-                  <h2>{greatWalk.location}</h2> <p>{greatWalk.difficulty}</p>{' '}
-                  <p>{greatWalk.duration}</p> <p>{greatWalk.distance}</p>{' '}
-                  <Link to={`/great-walks/${greatWalk.id}`}>
-                    {' '}
-                    <button>Read more</button>{' '}
-                  </Link>{' '}
-                </li>{' '}
+                      className="w-[300px] h-[200px]"
+                    />
+                  </Link>
+                  <div className="flex flex-col gap-4">
+                    <Link to={`/great-walks/${greatWalk.id}`}>
+                      <h1 className="text-[30px] font-bold">
+                        {greatWalk.name}
+                      </h1>
+                    </Link>
+                    <div className="text-[15px]">
+                      <h2>Location: {greatWalk.location}</h2>{' '}
+                      <p>Difficulty: {greatWalk.difficulty}</p>
+                      <p>Duration: {greatWalk.duration}</p>{' '}
+                      <p>Distance: {greatWalk.distance}</p>
+                    </div>
+                    <Link to={`/great-walks/${greatWalk.id}`}>
+                      <button className="bg-[#070446] text-[#d0f7a2] px-6 py-2 rounded-xl text-[15px] cursor-pointer">
+                        Read more
+                      </button>
+                    </Link>
+                  </div>
+                </li>
               </div>
-            ))}{' '}
-        </ul>{' '}
-      </div>{' '}
+            ))}
+        </ul>
+      </div>
     </>
   )
 }
