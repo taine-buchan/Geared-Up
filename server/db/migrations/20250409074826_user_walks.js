bbb572ad3e19d03dc73c13647f3d801db5d30013
Manasa <178882074+manasa-yanamandra@users.noreply.github.com>
2025-04-10 09:09:25 +1200

    working on user_walks seed file

--- server/db/migrations/20250409074826_user_walks.js
+++ server/db/migrations/20250409074826_user_walks.js
@@ -1,8 +1,8 @@
 export async function up(knex) {
   await knex.schema.createTable('user_walks', (table) => {
     table.increments('id').primary()
-    table.number('user_id').references('users.id').notNullable()
-    table.number('great_walk_id').references('great_walks.id')
+    table.integer('user_id').references('users.id')
+    table.integer('great_walk_id').references('great_walks.id')
     table.boolean('is_complete')
     table.boolean('is_planned')
   })
