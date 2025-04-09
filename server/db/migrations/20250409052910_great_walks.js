48cf12c91a40aab2d1e0f79ee63ba8086b3ac397
Manasa <178882074+manasa-yanamandra@users.noreply.github.com>
2025-04-09 18:22:57 +1200

    working on seed of great walks and migrations for the great walks completed

---
+++ server/db/migrations/20250409052910_great_walks.js
@@ -0,0 +1,20 @@
+export async function up(knex) {
+  await knex.schema.createTable('great_walks', (table) => {
+    table.increments('id').primary()
+    table.string('name').notNullable()
+    table.string('difficulty').notNullable()
+    table.string('elevation')
+    table.string('duration')
+    table.string('distance')
+    table.string('location')
+    table.text('description')
+    table.string('seasonal')
+    table.string('track_image_url')
+    table.string('doc_link')
+    table.string('required_equipment')
+  })
+}
+
+export async function down(knex) {
+  await knex.schema.dropTable('great_walks')
+}
