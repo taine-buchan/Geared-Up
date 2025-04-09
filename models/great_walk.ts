33a17d30dc82b006daa9e304c3b22ead2a769606
Manasa <178882074+manasa-yanamandra@users.noreply.github.com>
2025-04-09 21:21:51 +1200

    update great_walks model

--- models/great_walk.ts
+++ models/great_walk.ts
@@ -9,7 +9,61 @@ export interface GreatWalkData {
   seasonal: string
   trackImageUrl: string
   docLink: string
-  requiredEquipment: string
+  requiredEquipment: {
+    backpack: boolean
+    waterproofPackLiner: boolean
+    sleepingBag: boolean
+    firstAidKit: boolean
+    survivalKit: boolean
+    safetyEquipment: boolean
+    torchFlashlight: boolean
+    rubbishBag: boolean
+    bookingConfirmationAndId: boolean
+    earplugsForBunkrooms: boolean
+    drinkBottle: boolean
+    eatingAndCookingUtensils: boolean
+    gasCookerAndFuel: boolean
+    matchesOrLighter: boolean
+    generalToiletries: boolean
+    backupToiletOption: boolean
+    tent: boolean
+    sleepingMat: boolean
+    groundSheet: boolean
+    walkingClothes: boolean
+    hikingBoots: boolean
+    socks: boolean
+    shorts: boolean
+    shirt: boolean
+    underLayers: boolean
+    midLayers: boolean
+    raincoat: boolean
+    overtrousers: boolean
+    warmHatAndGloves: boolean
+    sunhatAndSunglasses: boolean
+    extraSocksUnderwearAndShirt: boolean
+    gaiters: boolean
+    lightweightShoesForHuts: boolean
+    carryFood: boolean
+    lightweightFood: boolean
+    emergencyFood: boolean
+    foodStorage: boolean
+    emergencyShelter: boolean
+    distressBeacon: boolean
+    cookingFacilities: boolean
+    sanitaryBins: boolean
+    gasCooker: boolean
+    fireStarters: boolean
+    lifeJacket: boolean
+    kayakOrCanoe: boolean
+    paddles: boolean
+    plasticDrumsOrEquivalent: boolean
+    dryBags: boolean
+    swimwear: boolean
+    sandalsOrAquaShoes: boolean
+    portableStoveAndFuel: boolean
+    candles: boolean
+    docConfirmationLetter: boolean
+  }
 }
 
 export interface GreatWalk extends GreatWalkData {
