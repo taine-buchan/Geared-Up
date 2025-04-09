059c103e0b708bfe940daaf14bcb2ae475d2fc93
Manasa <178882074+manasa-yanamandra@users.noreply.github.com>
2025-04-09 20:51:32 +1200

    update users seed file and user_walks seed file

--- server/db/seeds/user_walks.js
+++ server/db/seeds/user_walks.js
@@ -1,61 +1,61 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.seed = async function (knex) {
+export async function seed(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
+  await knex('user_walks').del()
+  await knex('user_walks').insert([
     {
       user_id: 1,
       great_walk_id: 1,
-      is_complete: false,
+      is_complete: true,
     },
     {
       user_id: 2,
       great_walk_id: 2,
-      is_complete: false,
+      is_complete: true,
     },
     {
       user_id: 3,
       great_walk_id: 3,
-      is_complete: false,
+      is_complete: true,
     },
     {
       user_id: 4,
       great_walk_id: 4,
-
-      is_complete: false,
+      is_complete: true,
     },
     {
       user_id: 5,
       great_walk_id: 5,
-      is_complete: false,
+      is_complete: true,
     },
     {
       user_id: 6,
       great_walk_id: 6,
-      is_complete: false,
+      is_complete: true,
     },
     {
       user_id: 7,
       great_walk_id: 7,
-      is_complete: false,
+      is_complete: true,
     },
     {
       user_id: 8,
       great_walk_id: 8,
-      is_complete: false,
+      is_complete: true,
     },
     {
       user_id: 9,
       great_walk_id: 9,
-      is_complete: false,
+      is_complete: true,
     },
     {
       user_id: 10,
       great_walk_id: 10,
-      is_complete: false,
+      is_complete: true,
+    },
+    {
+      user_id: 11,
+      great_walk_id: 11,
+      is_complete: true,
     },
   ])
 }
