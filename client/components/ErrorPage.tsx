44d3026a219c8e740c97414af4ce33310c2ff9d8
Gerry <193547099+gerard-dunn-25@users.noreply.github.com>
2025-04-10 14:13:38 +1200

    Created error page

--- client/components/ErrorPage.tsx
+++ client/components/ErrorPage.tsx
@@ -1,3 +1,26 @@
+import { Link } from 'react-router-dom'
+import Nav from './Nav'
+
 export default function ErrorPage() {
-  return <div>ErrorPage</div>
+  return (
+    <div>
+      <div>
+        <Nav />
+      </div>
+
+      <div className="flex items-center flex-col gap-8 p-8">
+        <img
+          className="rounded-full w-[10%]"
+          src={'/public/wired-lineal-1140-error-hover-oscillate.webp'}
+          alt="error symbol"
+        />
+        <p className="text-[#d0f7a2] font-bold" aria-label="error message">
+          Something went wrong...
+        </p>
+        <button className="button" aria-label="button to home page">
+          <Link to={'/'}>Home</Link>
+        </button>
+      </div>
+    </div>
+  )
 }
