fea78d6afe8b4415f640da55c33289f99bcf6547
Gerry <193547099+gerard-dunn-25@users.noreply.github.com>
2025-04-10 17:12:04 +1200

    adding loading indicator component, updated tests

--- client/components/ErrorPage.tsx
+++ client/components/ErrorPage.tsx
@@ -11,7 +11,7 @@ export default function ErrorPage() {
       <div className="flex items-center flex-col gap-8 p-8">
         <img
           className="rounded-full w-[10%]"
-          src={'/public/wired-lineal-1140-error-hover-oscillate.webp'}
+          src={'/wired-lineal-1140-error-hover-oscillate.webp'}
           alt="error symbol"
         />
         <p className="text-[#d0f7a2] font-bold" aria-label="error message">
