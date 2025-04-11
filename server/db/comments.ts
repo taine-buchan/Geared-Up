6a12cdc4b4c0e1c787bb5b10f3c7cd3e9deafa3b
Daisy Choi <chj15937@gmail.com>
2025-04-11 18:51:43 +1200

    fixed the issue for get comments

--- server/db/comments.ts
+++ server/db/comments.ts
@@ -3,10 +3,10 @@ import connection from './connection'
 export async function getCommentsByGreatWalkId(id: number) {
   return await connection('comments')
     .join('users', 'users.id', 'comments.user_id')
-    .join('great_walk', 'great_walk.id', 'comments.great_walk_id')
+    .join('great_walks', 'great_walks.id', 'comments.great_walk_id')
     .where('great_walk_id', id)
     .select(
-      'id',
+      'comments.id as id',
       'users.name as username',
       'great_walk_id as greatWalkId',
       'comment',
