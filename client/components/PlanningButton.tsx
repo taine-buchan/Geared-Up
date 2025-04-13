e9e73b803bea1e9a542902a8c32594eb80625380
Calum Wicks <calumwicks@gmail.com>
2025-04-13 15:57:14 +1200

    updating planning button

--- client/components/PlanningButton.tsx
+++ client/components/PlanningButton.tsx
@@ -5,15 +5,26 @@ import { useNavigate, useParams } from 'react-router-dom'
 import { useUserWalks } from '../hooks/useUserWalks.ts'
 
 function PlanningButton(token: string) {
+  const { user, loginWithRedirect } = useAuth0()
   const { walkId } = useParams()
   const navigate = useNavigate()
   const addWalk = useUserWalks(token)
+
+  const handleSignIn = () => {
+    if (user) {
+      console.log(user.sub)
+      return navigate(`/user/${user.sub}`)
+    } else loginWithRedirect()
+  }
 
   return (
     <>
       <IfAuthenticated>
         <Button onClick={() => addWalk.mutate(+walkId)}>Plan this Walk</Button>
       </IfAuthenticated>
+      {/* <IfNotAuthenticated> */}
+      <Button onClick={handleSignIn}>Sign in to Plan</Button>
+      {/* </IfNotAuthenticated> */}
     </>
   )
 }
