03d1151cb398cf6e1a81a25d033fb50441ddde53
Calum Wicks <calumwicks@gmail.com>
2025-04-14 16:19:49 +1200

    added component to greatWalk.tsx

--- client/components/PlanningButton.tsx
+++ client/components/PlanningButton.tsx
@@ -1,5 +1,5 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { IfAuthenticated } from './Authenticated.tsx'
+import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
 import Button from './Button.tsx'
 import { useNavigate, useParams } from 'react-router-dom'
 import { usePlannedWalks } from '../hooks/useUserWalks.ts'
@@ -14,21 +14,28 @@ function PlanningButton() {
     return null
   }
 
-  // const handleSignIn = () => {
-  //   if (user) {
-  //     console.log(user.sub)
-  //     return navigate(`/user/${user.sub}`)
-  //   } else loginWithRedirect()
-  // }
+  const handleSignIn = () => {
+    if (user) {
+      console.log(user.sub)
+      return navigate(`/user/${user.sub}`)
+    } else loginWithRedirect()
+  }
 
   return (
     <>
       <IfAuthenticated>
-        <Button onClick={() => addWalk.mutate(+id)}>Plan this Walk</Button>
+        <Button
+          onClick={() => addWalk.mutate(+id)}
+          className="button cursor-pointer"
+        >
+          Plan this Walk
+        </Button>
       </IfAuthenticated>
-      {/* <IfNotAuthenticated> */}
-      {/* <Button onClick={handleSignIn}>Sign in to Plan</Button> */}
-      {/* </IfNotAuthenticated> */}
+      <IfNotAuthenticated>
+        <Button onClick={handleSignIn} className="button cursor-pointer">
+          Sign in
+        </Button>
+      </IfNotAuthenticated>
     </>
   )
 }
