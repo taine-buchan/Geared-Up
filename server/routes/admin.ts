c1d65297b26f620e323cec39fa72018a3e5efce9
Manasa <178882074+manasa-yanamandra@users.noreply.github.com>
2025-04-14 12:31:05 +1200

    server side setup admin role complete

--- server/routes/admin.ts
+++ server/routes/admin.ts
@@ -1,41 +1,14 @@
 import { Router } from 'express'
-// import * as OpenIDConnect from 'express-openid-connect'
+import { requiresPermissionAuth0, validateAccessToken } from '../auth0.ts'
 
-import {
-  oidcConfig,
-  requiresPermission,
-  // validateAccessToken,
-} from '../auth0.ts'
-import { auth as oidc, requiresAuth } from 'express-openid-connect'
-
-// import { checkAdmin } from '../db/adminOnly.ts'
-// import connection from '../db/connection.ts'
-// const requiresAuth = OpenIDConnect.requiresAuth
-// const oidc = OpenIDConnect.auth
 const router = Router()
-router.use(oidc(oidcConfig))
-// Protected route: Only accessible to users with 'delete:comments' permission
+
 router.get(
   '/all-users',
-  // // validateAccessToken,
-  // requiresPermission('delete:comments'),
-  // (req, res) => {
-  //   // const users = await connection('users').select()
-  //   res.send('admin yipee')
-  //   // res.json(users)
-  requiresAuth(),
-  requiresPermission('delete:comments'),
+  validateAccessToken,
+  requiresPermissionAuth0('delete:comments'),
   async (req, res) => {
-    res.send('admin yipee - you have permission to moderate comments')
-  },
-)
-// Another protected route requiring 'read:data' permission
-router.get(
-  '/protected',
-  requiresAuth(),
-  requiresPermission('read:data'),
-  (req, res) => {
-    res.send('You have access!')
+    res.send(`you have a 'delete:comments' permission yipee`)
   },
 )
 
